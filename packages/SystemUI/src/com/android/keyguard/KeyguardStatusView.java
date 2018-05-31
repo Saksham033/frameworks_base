53fd6719051f7e8a861043b63eb4cd4083c14883
Shreyansh Lodha <slodha96@gmail.com>
2018-05-31 19:39:06 +0000

    lockscreen : Add small size style [1/2]
    Change-Id: I9917a1d6c12f71161d1f74ccb021d89154f3339e
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -243,7 +243,10 @@ public class KeyguardStatusView extends GridLayout {
         } else if (mClockSelection == 5) {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
-        } else {
+        } else if (mClockSelection == 6) {
+	    mClockView.setFormat12Hour(Html.fromHtml("<strong>h:mm</strong>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk:mm</strong>"));
+	} else {
             mClockView.setFormat12Hour("hh\nmm");
             mClockView.setFormat24Hour("kk\nmm");
         }
@@ -349,6 +352,7 @@ public class KeyguardStatusView extends GridLayout {
                 mDeadPoolClockView.setVisibility(View.GONE);
                 break;
             case 1: // digital (bold)
+	    case 6: // digital (small)
                 mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
                 mAnalogClockView.setVisibility(View.GONE);
                 mDeadPoolClockView.setVisibility(View.GONE);
@@ -364,11 +368,7 @@ public class KeyguardStatusView extends GridLayout {
                 mClockView.setVisibility(View.GONE);
                 break;
             case 4: // sammy
-                mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
-                mAnalogClockView.setVisibility(View.GONE);
-                mDeadPoolClockView.setVisibility(View.GONE);
-                break;
-            case 5: // sammy (bold)
+	    case 5: // sammy (bold)
                 mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
                 mAnalogClockView.setVisibility(View.GONE);
                 mDeadPoolClockView.setVisibility(View.GONE);
@@ -396,6 +396,15 @@ public class KeyguardStatusView extends GridLayout {
                 Settings.System.LOCKSCREEN_DATE_SELECTION, 0, UserHandle.USER_CURRENT);
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mKeyguardStatusArea.getLayoutParams();
+
+	if (mClockSelection == 6) {
+	    mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimensionPixelSize(R.dimen.widget_small_font_size));
+	} else {
+	    mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
+	}
+
         switch (mClockSelection) {
             case 0: // default digital
             default:
@@ -405,6 +414,7 @@ public class KeyguardStatusView extends GridLayout {
                 mDeadPoolClockView.unregisterReceiver();
                 break;
             case 1: // digital (bold)
+	    case 6: // digital (smal)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
                 mAnalogClockView.unregisterReceiver();
@@ -421,12 +431,7 @@ public class KeyguardStatusView extends GridLayout {
                 mDeadPoolClockView.registerReceiver();
                 break;
             case 4: // sammy
-                params.addRule(RelativeLayout.BELOW, R.id.clock_view);
-                mClockView.setSingleLine(false);
-                mAnalogClockView.unregisterReceiver();
-                mDeadPoolClockView.unregisterReceiver();
-                break;
-            case 5: // sammy (bold)
+	    case 5: // sammy (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
                 mAnalogClockView.unregisterReceiver();
